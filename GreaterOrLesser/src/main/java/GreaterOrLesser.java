a54c5955a5b86f8305ef06a0be5f5b15d1fd42ea
Racheal Amuneke <justgetmailing@yahoo.com>
2022-12-26 14:33:30 +0000

    Saved progress on GreaterOrLesser 2022-12-26 14:33:30 +0000

--- GreaterOrLesser/src/main/java/GreaterOrLesser.java
+++ GreaterOrLesser/src/main/java/GreaterOrLesser.java
@@ -1,3 +1,4 @@
+import javax.lang.model.util.ElementScanner6;
 
 public class GreaterOrLesser {
     /**
@@ -10,6 +11,16 @@ public class GreaterOrLesser {
      *          If neither is true, return "Equal". This problem is case-sensitive!
      */
     public String decide(int a, int b){
+        if(a < b){
+        return "Lesser";
+        }
+        if(a > b){
+        return "Greater";
+        }
+    else{
         return "Equal";
+        }
+      
     }
+
 }
