d9c684e259c9a20994e490adebdbce81f0f6eba9
Racheal Amuneke <justgetmailing@yahoo.com>
2022-12-29 01:04:01 +0000

    Saved progress on ForLoop 2022-12-29 01:04:01 +0000

--- ForLoop/src/main/java/ForLoop.java
+++ ForLoop/src/main/java/ForLoop.java
@@ -41,8 +41,12 @@ public class ForLoop {
      */
     public String returnNumbers(int start, int end){
         String buildingString = "";
+        for(int i = start;
+        i < end;
+        i++){
+        buildingString = buildingString + i + " ";
 
-//        code here
+        }
 
         return buildingString;
     }
