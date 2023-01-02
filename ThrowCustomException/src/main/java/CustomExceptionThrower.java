f5b281cb8eb65c521d4a797262ceee5db7672aef
Racheal Amuneke <justgetmailing@yahoo.com>
2023-01-02 20:39:12 +0000

    Saved progress on SetAllArrayElementsToN 2023-01-02 20:39:12 +0000

--- ThrowCustomException/src/main/java/CustomExceptionThrower.java
+++ ThrowCustomException/src/main/java/CustomExceptionThrower.java
@@ -9,11 +9,11 @@ public class CustomExceptionThrower {
      * I recommend looking at the test case for a good example of try/catch block usage, as well.
      */
     public void throwCustomException(){
-        try {
-            throw new CustomException();
-        } catch (CustomException e) {
-            System.out.println("Caught custom exception.");
-        }
+        
+            throwCustomException();
+        
+        
+        
 
     }
 
