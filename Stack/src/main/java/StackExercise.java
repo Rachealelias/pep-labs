108a1c37e224982ec2acea29683613735e8db1e0
Racheal Amuneke <justgetmailing@yahoo.com>
2023-01-05 20:28:03 +0000

    Saved progress on Average 2023-01-05 20:28:03 +0000

--- Stack/src/main/java/StackExercise.java
+++ Stack/src/main/java/StackExercise.java
@@ -12,14 +12,7 @@ import java.util.ArrayDeque;
  * methods): https://docs.oracle.com/javase/7/docs/api/java/util/Stack.html
  */
 public class StackExercise {
-    /**
-     * Implement and return some class that implements Deque, such as ArrayDeque, which contains all the functionality
-     * of a Stack. This challenge will not be using the Stack class, because the Stack class is known to have a shoddy
-     * implementation as it hasn't been changed since the very earliest version of Java. Deques were made to exhibit
-     * the exact same behavior and are generally better.
-     *
-     * @return a newly instantiated Deque. Deques implement both stack and queue behavior.
-     */
+   
     public Deque<String> createNewStack(){
         Deque<String> stk = new ArrayDeque<String>();
         return stk;
@@ -33,29 +26,16 @@ public class StackExercise {
         return stack.size();
     }
 
-    /**
-     * Add a value to the top of the deque.
-     * @param stack A Deque of Strings.
-     * @param item a String to be added to the deque.
-     */
+    
     public void addToTopOfStack(Deque<String> stack, String item){
         stack.addFirst(item);
     }
 
-    /**
-     * Remove the value from the top of the deque and return that value.
-     * (The method used to remove items from a stack also returns the value.)
-     * @param stack A Deque of Strings.
-     * @return the value popped from the top of the deque.
-     */
+    
     public String removeFromTopOfStack(Deque<String> stack){
         return stack.pop();
     }
-    /**
-     * Return the value at the top of a deque without removing the value.
-     * @param stack A Deque of Strings.
-     * @return the value peeked from the top of the deque.
-     */
+    
     public String getTopOfStackWithoutRemoving(Deque<String> stack){
         return stack.peek();
     }
